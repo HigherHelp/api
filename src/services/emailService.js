b4e4637f718bf1d13badcfd2a91407e505071b89
Jaden Hinton <HintonJaden05@gmail.com>
2024-06-14 15:17:09 -0400

    adding email service & instialltion of mail gun

---
+++ src/services/emailService.js
@@ -0,0 +1,89 @@
+import _ from 'lodash-es';
+import validator from 'validator';
+import dotenv from 'dotenv';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+import Logger from '../tools/logger.js';
+import { EmailServiceError } from '../constants/commonErrors.js';
+
+dotenv.config();
+
+let EMAIL_CLIENT;
+
+function setupClient() {
+  if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+    //use logger instead of console
+    Logger.error(`
+      Email Service: The following environment variables are required:
+        - MAILGUN_API_KEY
+        - MAILGUN_DOMAIN
+      You can run the API without this but you will not be able to send emails.
+      `);
+    return;
+  }
+  if (!EMAIL_CLIENT) {
+    const mailgun = new Mailgun(formData);
+    EMAIL_CLIENT = mailgun.client({
+      username: 'api',
+      key: process.env.MAILGUN_API_KEY,
+    });
+  }
+}
+
+function isValidString(string) {
+  return _.isString(string) && !_.isEmpty(string.trim());
+}
+
+// platform - API
+// Lnaguage - js
+// conventions  &best practices - libraries (lodash), validation (are parameters valid), good functions
+
+// function should always do one thing, only
+
+function validateText(text) {
+  if (!isValidString(text)) {
+    throw new EmailServiceError(`Cannot send email without valid text`);
+  }
+}
+function validateHtml(html) {
+  if (!isValidString(html)) {
+    throw new EmailServiceError(`Cannot send email without valid html`);
+  }
+}
+function validateSubject(subject) {
+  if (!isValidString(subject)) {
+    throw new EmailServiceError(`Cannot send email without valid subject`);
+  }
+}
+function validateRecipient(to) {
+  if (!isValidString(to) || !validator.isEmail(to.trim())) {
+    throw new EmailServiceError(`Cannot send email without valid recipient`);
+  }
+}
+
+//always use async if using 'await'
+export async function sendEmail({ to, subject, text }) {
+  validateSubject(subject);
+  validateText(text);
+  validateRecipient(to);
+  setupClient();
+  await EMAIL_CLIENT.messages.create(process.env.MAILGUN_DOMAIN, {
+    from: `Hello@${process.env.MAILGUN_DOMAIN}`,
+    to: to,
+    subject: subject,
+    text: text,
+  });
+}
+
+export async function sendHtmlEmail({ to, subject, html }) {
+  validateSubject(subject);
+  validateHtml(html);
+  validateRecipient(to);
+  setupClient();
+  await EMAIL_CLIENT.messages.create(process.env.MAILGUN_DOMAIN, {
+    from: `Hello@${process.env.MAILGUN_DOMAIN}`,
+    to: to,
+    subject: subject,
+    html: html,
+  });
+}
