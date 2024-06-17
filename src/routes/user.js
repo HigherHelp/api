564bbc0be5eb56ac0774d537f951187048ef0226
Kevin Huang <kevinhuang@Kevins-MBP.home>
2024-06-17 14:31:15 -0400

    fixed error handling messages with try catch

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,58 +13,6 @@ router.get('/', (request, response) => {
   response.json({ message: 'Welcome!' });
 });
 
-//gets a user from id
-router.get('/:id', async (request, response) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: request.params.id,
-    },
-  });
-  response.json({ user });
-});
-
-//update user by id
-router.patch('/:id', async (request, response) => {
-  const user = await prisma.user.findFirst({
-    where: {
-      id: request.params.id,
-    },
-  });
-  if (!user) {
-    throw new RequestError('User not found with id');
-  }
-  //const updateName =
-  await prisma.user.update({
-    where: {
-      id: request.params.id,
-    },
-    data: {
-      name: request.body.name,
-    },
-  });
-  response.json({ message: 'updated' });
-
-  //const updateEmail =
-  await prisma.user.update({
-    where: {
-      id: request.params.id,
-    },
-    data: {
-      email: request.body.email,
-    },
-  });
-});
-
-router.delete('/:id', async (request, response) => {
-  //const user =
-  await prisma.user.delete({
-    where: {
-      id: request.params.id,
-    },
-  });
-  response.json({ message: 'deletion successful' });
-});
-
 router.get('/archived', () => {
   throw new ArchivedError('This user has been archived');
 });
@@ -75,6 +23,84 @@ router.get('/error', () => {
 
 router.get('/auth', () => {
   throw new AuthenticationError('Authentication Error');
+});
+
+//gets a user from id
+router.get('/:id', async (request, response, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (!user) {
+      throw new RequestError('User not found with ID');
+    }
+    response.json({ user });
+  } catch (error) {
+    next(error);
+    return;
+  }
+});
+
+//update user by id
+router.patch('/:id', async (request, response, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (!user) {
+      throw new RequestError('User not found with ID');
+    }
+    //const updateName =
+    await prisma.user.update({
+      where: {
+        id: request.params.id,
+      },
+      data: {
+        name: request.body.name,
+      },
+    });
+    response.json({ message: 'updated' });
+
+    //const updateEmail =
+    await prisma.user.update({
+      where: {
+        id: request.params.id,
+      },
+      data: {
+        email: request.body.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+    return;
+  }
+});
+
+router.delete('/:id', async (request, response, next) => {
+  try {
+    const testUser = await prisma.user.findUnique({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (!testUser) {
+      throw new RequestError('User not found with ID');
+    }
+    //const user =
+    await prisma.user.delete({
+      where: {
+        id: request.params.id,
+      },
+    });
+    response.json({ message: 'deletion successful' });
+  } catch (error) {
+    next(error);
+    return;
+  }
 });
 
 export default router;
