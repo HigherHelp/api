e94d3552da46e758ede609073387d2c1e3c69fcc
Pablo Najera <pablonajera@users.noreply.github.com>
2024-06-18 21:10:49 -0400

    adding prisma seed functionality (#9)
    Co-authored-by: Pablo Najera <pablo@najera.cc>

---
+++ prisma/seed/index.js
@@ -0,0 +1,51 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+async function clearDatabase() {
+  await prisma.refreshTokens.deleteMany();
+  await prisma.user.deleteMany();
+}
+
+async function main() {
+  await clearDatabase();
+  const users = [];
+  const refreshTokens = [];
+
+  for (let i = 0; i < 10; i++) {
+    const email = faker.internet.email();
+    const name = faker.person.fullName();
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+    users.push(user);
+
+    // Each user will have between 0 to 3 refresh tokens
+    const numberOfTokens = faker.number.int({ min: 0, max: 3 });
+    for (let j = 0; j < numberOfTokens; j++) {
+      const refreshToken = await prisma.refreshTokens.create({
+        data: {
+          ipAddress: faker.internet.ip(),
+          userAgent: faker.internet.userAgent(),
+          userId: user.id,
+        },
+      });
+      refreshTokens.push(refreshToken);
+    }
+  }
+
+  console.log(`${users.length} users created.`);
+  console.log(`${refreshTokens.length} refresh tokens created.`);
+}
+
+try {
+  await main();
+} catch (error) {
+  console.error(error);
+  throw new Error('Could not seed the database');
+}
+await prisma.$disconnect();
