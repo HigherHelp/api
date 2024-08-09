13c4f7e52886cddb2f00ddb150756c3150eeb73e
Jaden Hinton <Hintonjaden05@gmail.com>
2024-08-09 12:57:07 -0400

    Jaden/Sign-In (#11)
    * Added Sign In route
    
    * Updated Sign-In endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import Prisma from '../tools/prisma.js';
 import validator from 'validator';
 import { RequestError } from '../constants/commonErrors.js';
+import tokenService from '../services/tokenService.js';
 
 const router = express.Router();
 
@@ -40,10 +41,52 @@ router.post('/signup', async (request, response, next) => {
       },
     });
 
-    // respond with a success message
     response.json({
       message: 'Created user in database!',
       user: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/signin', async (request, response, next) => {
+  try {
+    const userEmail = request.body.email;
+    const userPassword = request.body.password;
+
+    if (!userEmail || !validator.isEmail(userEmail)) {
+      throw new RequestError('Must provide a valid email');
+    }
+    if (!userPassword || !userPassword.trim()) {
+      throw new RequestError('Must provide a valid password');
+    }
+
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+    const passwordMatch = await bcrypt.compare(userPassword, user.password);
+    if (!user && !passwordMatch) {
+      throw new RequestError('Invalid email or password');
+    }
+
+    const accessToken = await tokenService.createAccessToken(user);
+    const refreshToken = await tokenService.getSignedRefreshToken({
+      request,
+      user,
+    });
+
+    response.json({
+      message: 'Successfully signed in!',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     });
   } catch (error) {
     next(error);
